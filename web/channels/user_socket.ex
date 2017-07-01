558c5188ac56a7bdc68407e84127a36370ca8b65
boilercoding <boilercoding@gmail.com>
2017-07-01 14:34:47 -0400

    Adds poll channel

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule Haypoll.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Haypoll.RoomChannel
+  channel "polls:*", Haypoll.PollChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
