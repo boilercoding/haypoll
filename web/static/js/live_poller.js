19197a7179906c309ae34860356e3544973111f7
boilercoding <boilercoding@gmail.com>
2017-07-01 15:25:25 -0400

    Adds push and broadcast of votes

--- web/static/js/live_poller.js
+++ web/static/js/live_poller.js
@@ -8,6 +8,8 @@ export class LivePoller {
     if (!$("#poll-id").length) { return }
     // Set up our channel for Polls
     let pollChannel = this._setupPollChannel()
+    // Set up each of the voting buttons
+    this._setupVoteButtons(pollChannel)
   }
   _createSocket() {
     // Open up a new websocket connection
@@ -28,6 +30,13 @@ export class LivePoller {
     // Next, specify that we want to join a polls channel of the polls: with the poll id.
     // Remember our code in PollChannel.ex that looked like: "polls:" <> poll_id
     let pollChannel = socket.channel("polls:" + pollId)
+
+    // Set up a handler for when the channel receives a new_vote message
+    pollChannel.on("new_vote", vote => {
+      // Update the voted item’s display
+      this._updateDisplay(vote.entry_id)
+    })
+
     // Finally, join the channel we created. On success, let the console know that we joined.
     // On failure, tell us why it errored out.
     pollChannel
@@ -37,5 +46,42 @@ export class LivePoller {
     // Finally, return the whole channel we've created; we'll need that to push
     // messages out later.
     return pollChannel
+  }
+
+  _updateDisplay(entryId) {
+   // Store the current object for use later
+   let self = this
+   // Iterate over each entry
+   $.each($("li.entry"), (index, item) => {
+     // Store the current item
+     let li = $(item)
+     // If the entry ids match, update the number of votes for that element
+     if (entryId == li.data("entry-id")) {
+       // Get the number of current votes, parse it as an integer, and add one
+       let newVotes = +(li.find(".votes").text()) + 1
+       // And update the display for that entry
+       this._updateEntry(li, newVotes)
+     }
+   })
+  }
+
+  _updateEntry(li, newVotes) {
+    // Find the .votes span and update it to whatever the new votes value is
+    li.find(".votes").text(newVotes)
+  }
+
+  _setupVoteButtons(pollChannel) {
+    // Set up our default click handler for votes
+    $(".vote").on("click", event => {
+      event.preventDefault()
+      // Find the containing list item
+      let li = $(event.currentTarget).parents("li")
+      // Grab the entry id for what the user voted on
+      let entryId = li.data("entry-id")
+      // And the current poll
+      let pollId = $("#poll-id").val()
+      // And then push a new_vote message with the entry id onto the channel
+      pollChannel.push("new_vote", { entry_id: entryId })
+    })
   }
 }
